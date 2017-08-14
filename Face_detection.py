18e4eb781d1b1550aa355c8082e0238bf7b34a3f
AndrewM <amourcos@gmail.com>
2017-08-13 20:04:31 -0400

    Added watermark

--- Face_detection.py
+++ Face_detection.py
@@ -1,22 +1,31 @@
+# include library for computer vision
 import cv2
 
+# include Haar Cascade (trained sample data) to compare to faces
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# include "filter"
 helmet = cv2.imread('helmet.png')
 
-
+# get video input
 cap = cv2.VideoCapture(0)
 
 while 1:
+    # get frames
     ret, img = cap.read()
+    # convert frames to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    try:
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    except:
-        pass
 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, 'par Andrew Mourcos', (0, 15), font, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+    # detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # iterate over faces found
     for (x, y, w, h) in faces:
         h = h+150
-        w= w+150
+        w = w+150
         roi_color = img[y-100:y -100 + h, x:x + w]
         resize_helmet = cv2.resize(helmet, (w,h), interpolation=cv2.INTER_AREA)
 
@@ -36,5 +45,3 @@ while 1:
         if k == 27:
             cap.release()
             cv2.destroyAllWindows()
-
-
